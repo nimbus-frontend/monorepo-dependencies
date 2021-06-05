4c23979835fdab1d96d1f70b1e69cfeec8816910
Alexey Taranovskiy <altaranovskiy@gmail.com>
2021-06-05 02:38:26 +0000

    Feat regenerate apps

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: [],
+  projects: [
+    '<rootDir>/apps/frontend/website-client',
+    '<rootDir>/apps/frontend/app-client',
+    '<rootDir>/apps/backend/api-gateway',
+    '<rootDir>/apps/backend/api-proxy',
+    '<rootDir>/apps/backend/socket-server',
+  ],
 };
